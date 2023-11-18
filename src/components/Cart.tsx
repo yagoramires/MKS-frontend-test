aaa8cb7f1145bc34fa1d6bbea8efe1c0c3e619bf
yagoramires <yago.ramiresx@gmail.com>
2023-11-18 20:11:42 -0300

    feat: cart component footer

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,6 +6,7 @@ import styles from "../styles/cart.module.scss";
 import { TiShoppingCart } from "react-icons/ti";
 import { IoIosClose } from "react-icons/io";
 import ProductCartCard from "./ProductCartCard";
+import Link from "next/link";
 
 type Props = {};
 
@@ -15,25 +16,25 @@ export default function Cart() {
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
-    if (!isOpen ) {
-
+    if (!isOpen) {
       controls.start({ x: isOpen ? "-100%" : 0 });
     } else {
-      controls.start({ x: !isOpen ? 0 : '100%' });
+      controls.start({ x: !isOpen ? 0 : "100%" });
     }
   };
 
   const product = {
-    "id": 8,
-    "name": "Headset Cloud Stinger",
-    "brand": "HyperX",
-    "description": "O HyperX Cloud Stinger™ é o headset ideal para jogadores que procuram leveza e conforto, qualidade de som superior e maior praticidade.",
-    "photo": "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/hyperxcloudstinger.webp",
-    "price": "600.00",
-    "createdAt": "2023-10-30T16:25:01.093Z",
-    "updatedAt": "2023-10-30T16:25:01.093Z"
-  }
-
+    id: 8,
+    name: "Headset Cloud Stinger",
+    brand: "HyperX",
+    description:
+      "O HyperX Cloud Stinger™ é o headset ideal para jogadores que procuram leveza e conforto, qualidade de som superior e maior praticidade.",
+    photo:
+      "https://mks-sistemas.nyc3.digitaloceanspaces.com/products/hyperxcloudstinger.webp",
+    price: "600.00",
+    createdAt: "2023-10-30T16:25:01.093Z",
+    updatedAt: "2023-10-30T16:25:01.093Z",
+  };
 
   return (
     <div>
@@ -54,8 +55,19 @@ export default function Cart() {
         </div>
 
         <ul className={styles.cartList}>
-        <ProductCartCard product={product}/>
+          <ProductCartCard product={product} />
+          <ProductCartCard product={product} />
         </ul>
+
+        <div className={styles.cartFooter}>
+          <p>Total: <span> {Number(product.price).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}</span></p>
+        <button>Finalizar Compra</button>
+        </div>
       </motion.div>
       <button onClick={toggleSidebar} className={styles.cartBtn}>
         <TiShoppingCart size={19} /> <span>0</span>
